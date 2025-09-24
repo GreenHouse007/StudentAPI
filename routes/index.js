5a1c750476556e6d910beb30641e7acfe9f7192c
GreenHouse007 <adventurebyalex@gmail.com>
2025-09-24 09:38:03 -0600

    authentication addition

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const myController = require("../controllers");
 routes.get("/", myController.awesomeFunction);
 routes.get("/ttech", myController.tooeleTechFunction);
 routes.use("/students", require("./students"));
+routes.use("/auth", require("./auth"));
 
 module.exports = routes;
